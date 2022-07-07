c5dcd85f4057cfd50633b13d7db3d6133c17cff6
md.rahman <md.rahman@mdrahmans-MacBook-Pro.local>
2022-07-07 08:21:29 +0530

    if else component

--- src/IfElse.js
+++ src/IfElse.js
@@ -1,5 +1,9 @@
-function IfElse() {
-    return <h1>IF Else</h1>;
+function IfElse({ cond, children }) {
+    if (!children || !Array.isArray(children)) throw new Error("If Else component should have two children");
+    // if (!children) children = [null, null];
+    const [first, second] = children;
+    // false, 0, "", NaN, null, undefined
+    return cond ? first : second;
 }
 
 export default IfElse;
--- src/Student.js
+++ src/Student.js
@@ -7,13 +7,10 @@ import './Student.css';
 function Student({ marks }) {
     return <>
         <h1>Student</h1>
-        <IfElse></IfElse>
-        <ShouldRender cond={marks >= 40}>
+        <IfElse cond={marks >= 40}>
             <h3 className="pass">Passed</h3>
-        </ShouldRender>
-        <ShouldRender cond={marks < 40}>
             <h3 className="fail">Failed</h3>
-        </ShouldRender>
+        </IfElse>
         {/* {marks >= 40 ? <h3>Passed</h3> : <h3>Failed</h3>} */}
         {/* <h3 className={marks >= 40 ? 'pass' : 'fail'}> {marks >= 40 ? 'Passed' : 'Failed'}</h3> */}
     </>
