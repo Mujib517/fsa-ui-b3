b72c8e53d9b1f3c463198b83cc3ec6e621c0b0da
Mujib <mujib517@gmail.com>
2022-07-21 08:38:22 +0530

    Validations

--- src/NewProduct.js
+++ src/NewProduct.js
@@ -35,6 +35,11 @@ class NewProduct extends React.Component {
         }
     }
 
+    isInvalid = () => {
+        const { brand, model, price } = this.state.product;
+        return !brand || !model || !price;
+    }
+
     render() {
         const { brand, model, price, inStock } = this.state.product;
         return <div className="col-md-4">
@@ -53,21 +58,30 @@ class NewProduct extends React.Component {
                 <label for="txtBrand" className="form-label">Brand</label>
                 <input id="txtBrand" name="brand" value={brand}
                     placeholder="Brand" className="form-control" type="text" onChange={this.onInputChange} />
+                <ShouldRender cond={!brand}>
+                    <span className="text-danger">Brand is required</span>
+                </ShouldRender>
             </div>
             <div className="m-3">
                 <label for="txtModel" className="form-label">Model</label>
                 <input id="txtModel" name="model" value={model} placeholder="Model" className="form-control" type="text" onChange={this.onInputChange} />
+                <ShouldRender cond={!model}>
+                    <span className="text-danger">Model is required</span>
+                </ShouldRender>
             </div>
             <div className="m-3">
                 <label for="txtPrice" className="form-label">Price</label>
                 <input id="txtPrice" name="price" value={price} placeholder="Price" className="form-control" type="text" onChange={this.onInputChange} />
+                <ShouldRender cond={!price}>
+                    <span className="text-danger">Price is required</span>
+                </ShouldRender>
             </div>
             <div className="m-3">
                 <label for="chkPrice" className="form-label m-1">InStock</label>
                 <input type="checkbox" name="inStock" value={inStock} id="chkPrice" className="m-1 form-check-input" onChange={this.onInputChange} />
             </div>
             <div className="m-3">
-                <button onClick={this.onSave} className="btn btn-success btn-sm">Save</button>
+                <button disabled={this.isInvalid()} onClick={this.onSave} className="btn btn-success btn-sm">Save</button>
             </div>
         </div>
     }
