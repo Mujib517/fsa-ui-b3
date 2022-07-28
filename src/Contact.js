45aeb372363e2d42ab552aaae25ff420d0f62519
Mujib <mujib517@gmail.com>
2022-07-28 09:04:13 +0530

    context api pre req

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
+// Flux
+// Redux: complex
+// Context API
+const Level2 = ({ phone, onNotify }) => <>
+    <h1>Level 2</h1>
+    <Level3 phone={phone} onNotify={onNotify} />
+</>
+
+const Level3 = ({ phone, onNotify }) => <>
+    <h1>Level 3 {phone}</h1>
+    <button onClick={() => onNotify()}>Notify</button>
+</>
+
+const Level1 = ({ phone, onNotify }) => <div>
+    <h1>Level 1</h1>
+    <Level2 phone={phone} onNotify={onNotify} />
+</div>
+
 function Contact() {
-    return (<h1>Contact Page</h1>);
+    const [phone] = useState('949595959');
+    const onNotify = () => console.log('notified');
+    return <div>
+        <Level1 phone={phone} onNotify={onNotify} />
+        <h1>Contact Page</h1>
+    </div>
 }
 
 export default Contact;
